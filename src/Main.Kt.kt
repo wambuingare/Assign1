75b6e4d60760626589730da9ae96dec197f552dc
Wambuingare <wambuinancyngare@gmail.com>
2022-03-01 09:49:59 +0300

    Variables

---
+++ src/Main.Kt.kt
@@ -0,0 +1,13 @@
+fun main (){
+    var name:String=("Nancy Wambui")
+    var age:Byte=(22)
+    var phoneNumber=("0725124209")
+    var weight:Int=(50)
+    var kenyan:Boolean=true
+
+println(name)
+        println(age)
+        println(phoneNumber)
+        println(weight)
+        println(kenyan)
+}
